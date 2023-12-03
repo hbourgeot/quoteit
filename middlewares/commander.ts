cbd6f5088678d2cf979d6e42dbb2da8eb5d8bf27
Henrry Bourgeot <henrrybrgt@gmail.com>
2023-12-02 21:46:18 -0400

    no nodejs

--- middlewares/commander.ts
+++
@@ -1,9 +0,0 @@
-import type { Context } from "telegraf";
-
-export function commander() {
-  return async (ctx: Context, next: () => Promise<void>) => {
-    if (ctx.updateType === 'message' && (ctx.chat?.type === 'group' || ctx.chat?.type === 'supergroup')) {
-      
-    }
-  }
-}
